76a9c4c9bb5d46656905f1f7d8ce43237c23bc71
Oleg <wowgameoleg@mail.ru>
2021-09-22 12:19:51 +0300

    Логин теперь отображается при переходе на InfoFragment

--- app/src/main/java/com/example/popularlibrariesapp/presenter/UsersPresenter.kt
+++ app/src/main/java/com/example/popularlibrariesapp/presenter/UsersPresenter.kt
@@ -1,14 +1,21 @@
 package com.example.popularlibrariesapp.presenter
 
+import android.content.Intent
+import android.os.Bundle
 import com.example.popularlibrariesapp.model.GitHubUser
 import com.example.popularlibrariesapp.model.GitHubUsersRepo
 import com.example.popularlibrariesapp.recyclerView.IUserListPresenter
 import com.example.popularlibrariesapp.recyclerView.UserItemView
+import com.example.popularlibrariesapp.screens.AndroidScreens
+import com.example.popularlibrariesapp.screens.IScreens
 import com.example.popularlibrariesapp.view.UsersView
 import com.github.terrakok.cicerone.Router
+import com.github.terrakok.cicerone.Screen
 import moxy.MvpPresenter
 
-class UsersPresenter(private val usersRepo: GitHubUsersRepo, val router: Router): MvpPresenter<UsersView>() {
+const val LOGIN_KEY = "LOGIN_KEY"
+
+class UsersPresenter(private val usersRepo: GitHubUsersRepo, val router: Router, val screens: IScreens): MvpPresenter<UsersView>() {
 
     class UsersListPresenter: IUserListPresenter {
         val users = mutableListOf<GitHubUser>()
@@ -30,6 +37,9 @@ class UsersPresenter(private val usersRepo: GitHubUsersRepo, val router: Router)
         loadData()
         usersListPresenter.itemClickListener = { itemView ->
             //переход на экран пользователя
+            val bundle = Bundle()
+            bundle.putString(LOGIN_KEY, usersRepo.getUsers()[itemView.pos].login)
+            router.navigateTo(screens.info(bundle), true)
         }
     }
 
